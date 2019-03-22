5368e77b27c9249ad84cf07b79d30f1fdc3e0084
Mark Smith <smith.h.mark@gmail.com>
2019-03-21 17:20:50 -0700

    handling two differenet versions of arg data

--- good-decisions-ui/src/Argument.js
+++ good-decisions-ui/src/Argument.js
@@ -16,9 +16,28 @@ const topHeader = (fcols, outcomes) => {
   }
   return <tr key={rid} id={rid}>{cells}</tr>
 }
+
+const topHeader2 = (fcols, outcomes) => {
+  let cells = [];
+  let row = 0;
+  let rid = rowid(row);
+
+  for (let col = 0; col < outcomes.length + fcols; col++) {
+    let cid = cellid(row, col);
+    if (col < fcols) {
+      cells.push(<th key={cid} id={cid}></th>);
+    } else {
+      cells.push(
+        <th key={cid} id={cid}>{outcomes[col - fcols].text}</th>);
+    }
+  }
+  return <tr key={rid} id={rid}>{cells}</tr>
+}
+
 const Fact = (props) => {
   if (props.src) {
-    return (<th key={props.key} id={props.id}>{props.children}, <a href={props.src}>source</a></th>)
+    return (
+      <th key={props.key} id={props.id}>{props.children}, <a href={props.src}>source</a></th>)
   } 
   return (<th>{props.children}</th>)
 }
@@ -39,8 +58,24 @@ const buildRow = (row, fact, inputs, factCols) => {
      {cells}
     </tr>
   )
-  //    <Fact src={facts[row].src}>{fact.text}</Fact>
-  //    {inputs.map((ip) => {return <Input>{ip}</Input>})}
+}
+const buildRow2 = (row, fact, inputs, factCols, colRelIds) => {
+  let cells = [];
+  for (let col = 0; col < colRelIds.length + factCols; col++) {
+    let cid = cellid(row, col);
+    if (col === 0) {
+      cells.push(<Fact key={cid} id={cid} src={fact.src}>{fact.text}</Fact>);
+    } else {
+      let crid = colRelIds[col - factCols]; 
+      cells.push(
+        <td key={cid} id={cid}>{inputs[crid]}</td>);
+    }
+  }
+  return (
+    <tr key={rowid(row)}>
+     {cells}
+    </tr>
+  )
 }
 
 const cellid = (row, col) => {
@@ -52,23 +87,64 @@ const rowid = (rownum) => {
 }
 
 const ArgumentV0dot1 = (props) => {
-    let rows = [];
-    let offset = 1; // how many cells left of the first outcome
-    let row = 0;
-    let udata = null;
-    if (props.user) {
-      udata = props.children.users[props.user];
+  let rows = [];
+  let offset = 1; // how many cells left of the first outcome
+  let row = 0;
+  let udata = null;
+  if (props.user) {
+    udata = props.children.users[props.user];
+  } else {
+    udata = props.children.users[props.children.creator.uid];
+  }
+  for (row = 0; row < props.children.facts.length + 1; row++) {
+    if (row === 0) {
+      rows.push(topHeader(offset, props.children.outcomes));
     } else {
-      udata = props.children.users[props.children.creator.uid];
+      rows.push(
+        buildRow(row, props.children.facts[row-1], udata[row-1], offset));
     }
-    for (row = 0; row < props.children.facts.length + 1; row++) {
-      if (row === 0) {
-        rows.push(topHeader(offset, props.children.outcomes));
-      } else {
-        rows.push(
-          buildRow(row, props.children.facts[row-1], udata[row-1], offset));
-      }
+  }
+  return (
+    <table>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+}
+
+const ArgumentV0dot2 = (props) => {
+  let rows = [];
+  let offset = 1; // how many cells left of the first outcome
+  let row = 0;
+  let udata = null;
+  if (props.user) {
+    udata = props.children.users[props.user];
+  } else {
+    udata = props.children.users[props.children.creator.uid];
+  }
+  let colRelIds = props.children.outcomes.map((o) => {
+    return o.relid;
+  });
+  let rowRelIds = props.children.facts.map((f) => {
+    return f.relid;
+  });
+  for (row = 0; row < props.children.facts.length + 1; row++) {
+    if (row === 0) {
+      rows.push(topHeader2(offset, props.children.outcomes));
+    } else {
+      let rrid = rowRelIds[row-1]
+      console.log("rendering row", row, "with rrid:", rrid);
+      console.log("inputs[rrid]=", udata[rrid]);
+      rows.push(
+        buildRow2(
+          row,
+          props.children.facts[rrid],
+          udata[rrid],
+          offset,
+          colRelIds));
     }
+  }
   return (
     <table>
       <tbody>
@@ -83,6 +159,8 @@ class Argument extends Component {
     let tbl = null;
     if (this.props.children.version === "0.1") {
       tbl = <ArgumentV0dot1 user={this.props.user}>{this.props.children}</ArgumentV0dot1>
+    } else if (this.props.children.version === "0.2") {
+      tbl = <ArgumentV0dot2 user={this.props.user}>{this.props.children}</ArgumentV0dot2>
     } else {
       tbl = <span>Yikes! I don't know how to render that!</span>
     }
