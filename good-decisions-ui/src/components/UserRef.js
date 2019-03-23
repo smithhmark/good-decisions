b9d14ee17f0dd930aa9f8ec5427ed6f2373bdf90
Mark Smith <smith.h.mark@gmail.com>
2019-03-23 14:26:37 -0700

    adding icky hack where userref has no uname

--- good-decisions-ui/src/components/UserRef.js
+++ good-decisions-ui/src/components/UserRef.js
@@ -3,6 +3,9 @@ import React from 'react';
 const UserRef = (props) => {
   let uid = props.children.uid;
   let uname = props.children.uname;
+  if (!uname) {
+    uname = uid; //!!! unpleasent
+  }
   if (props.link === true) {
     return (<span className="user-ref"><a href={"/user/"+uid}>{uname}</a></span>
     );
